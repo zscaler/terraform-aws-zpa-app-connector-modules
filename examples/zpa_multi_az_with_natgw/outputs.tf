5a476022266ae38cef036cced82f7c61c1abefcb
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-07-08 11:08:31 -0700

    Clean Up

--- examples/zpa_multi_az_with_natgw/outputs.tf
+++
@@ -1,6 +0,0 @@
-/*
-output "public_ips" {
-  description = "Map of public IPs created within the module."
-  value       = { for k, v in module.app_connector : k => v.public_ips }
-}
-*/
--- modules/zpa_app_connector_group/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "provisioning_key" {
-  value = zpa_provisioning_key.this.provisioning_key
-}
-  
