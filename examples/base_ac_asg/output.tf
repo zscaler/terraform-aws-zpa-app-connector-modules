f9f996c7af1320fb98616aaa15e621ed4352ffdd
Jameson Molnar <JMolnar@zscaler.com>
2022-09-11 20:51:38 -0400

    feat: add base_ac_asg deployment

--- examples/base_ac_asg/output.tf
+++ examples/base_ac_asg/output.tf
@@ -3,35 +3,36 @@ locals {
   testbedconfig = <<TB
 
 1) Copy the SSH key to the bastion host
-scp -i ${var.name_prefix}-key-${random_string.suffix.result}.pem ${var.name_prefix}-key-${random_string.suffix.result}.pem centos@${module.bastion.public_dns}:/home/centos/.
+scp -i ${var.name_prefix}-key-${random_string.suffix.result}.pem ${var.name_prefix}-key-${random_string.suffix.result}.pem ec2-user@${module.bastion.public_dns}:/home/ec2-user/.
 
 2) SSH to the bastion host
-ssh -i ${var.name_prefix}-key-${random_string.suffix.result}.pem centos@${module.bastion.public_dns}
+ssh -i ${var.name_prefix}-key-${random_string.suffix.result}.pem ec2-user@${module.bastion.public_dns}
 
 3) SSH to the App Connectors
-ssh -i ${var.name_prefix}-key-${random_string.suffix.result}.pem admin@<< AC mgmt IP >> -o "proxycommand ssh -W %h:%p -i ${var.name_prefix}-key-${random_string.suffix.result}.pem centos@${module.bastion.public_dns}"
+ssh -i ${var.name_prefix}-key-${random_string.suffix.result}.pem admin@<< AC mgmt IP >> -o "proxycommand ssh -W %h:%p -i ${var.name_prefix}-key-${random_string.suffix.result}.pem ec2-user@${module.bastion.public_dns}"
+Note: Due to the dynamic nature of autoscaling groups, you will need to login to the AWS console and identify the private IP for each AC deployed and insert into the above command replacing "<< AC mgmt IP >>"
 
-Note: Due to the dynamic nature of autoscaling groups, you will need to login to the AWS console and identify the mgmt IP for each AC deployed and insert into the above command replacing "<< AC mgmt IP >>"
-Note: You can also login to the App Connectors directly from the AWS Console via Session Manager.
-
-
-VPC: 
-${aws_vpc.vpc1.id}
+VPC:         
+${module.network.vpc_id}
 
 All AC AZs:
 ${join("\n", distinct(module.ac-asg.availability_zone))}
 
 All NAT GW IPs:
-${join("\n", aws_nat_gateway.ngw.*.public_ip)}
+${join("\n", module.network.nat_gateway_ips)}
+
+All AC IAM Role ARNs:
+${join("\n", module.ac-iam.iam_instance_profile_arn)}
 
 TB
 }
 
 output "testbedconfig" {
-  value = local.testbedconfig
+  value     = local.testbedconfig
+  sensitive = true
 }
 
 resource "local_file" "testbed" {
-  content = local.testbedconfig
-  filename = "testbed.txt"
+  content  = local.testbedconfig
+  filename = "../testbed.txt"
 }
--- examples/base_ac_asg/variables.tf
+++ examples/base_ac_asg/variables.tf
@@ -1,132 +1,132 @@
-# aws variables
-
 variable "aws_region" {
+  type        = string
   description = "The AWS region."
   default     = "us-west-2"
 }
 
 variable "name_prefix" {
+  type        = string
   description = "The name prefix for all your resources"
   default     = "zsdemo"
-  type        = string
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR"
+  type        = string
+  description = "VPC IP CIDR Range. All subnet resources that might get created (public / app connector) are derived from this /16 CIDR. If you require creating a VPC smaller than /16, you may need to explicitly define all other subnets via public_subnets and ac_subnets variables"
   default     = "10.1.0.0/16"
 }
 
+variable "public_subnets" {
+  type        = list(string)
+  description = "Public/NAT GW Subnets to create in VPC. This is only required if you want to override the default subnets that this code creates via vpc_cidr variable."
+  default     = null
+}
+
+variable "ac_subnets" {
+  type        = list(string)
+  description = "App Connector Subnets to create in VPC. This is only required if you want to override the default subnets that this code creates via vpc_cidr variable."
+  default     = null
+}
+
 variable "az_count" {
-  description = "Default number of subnets to create based on availability zone"
-  type = number
+  type        = number
+  description = "Default number of subnets to create based on availability zone input"
   default     = 2
   validation {
-          condition     = (
-          (var.az_count >= 1 && var.az_count <= 3)
-        )
-          error_message = "Input az_count must be set to a single value between 1 and 3. Note* some regions have greater than 3 AZs. Please modify az_count validation in variables.tf if you are utilizing more than 3 AZs in a region that supports it. https://aws.amazon.com/about-aws/global-infrastructure/regions_az/."
-      }
+    condition = (
+      (var.az_count >= 1 && var.az_count <= 3)
+    )
+    error_message = "Input az_count must be set to a single value between 1 and 3. Note* some regions have greater than 3 AZs. Please modify az_count validation in variables.tf if you are utilizing more than 3 AZs in a region that supports it. https://aws.amazon.com/about-aws/global-infrastructure/regions_az/."
+  }
 }
 
 variable "owner_tag" {
+  type        = string
   description = "populate custom owner tag attribute"
-  type = string
-  default = "zsac-admin"
+  default     = "zsac-admin"
 }
 
 variable "tls_key_algorithm" {
-  default   = "RSA"
-  type      = string
+  type        = string
+  description = "algorithm for tls_private_key resource"
+  default     = "RSA"
 }
 
-variable "ac_count" {
-  description = "Default number of App Connector appliances to create"
-  default     = 2
+variable "bastion_nsg_source_prefix" {
+  type        = list(string)
+  description = "CIDR blocks of trusted networks for bastion host ssh access"
+  default     = ["0.0.0.0/0"]
 }
 
 variable "acvm_instance_type" {
   description = "App Connector Instance Type"
   default     = "m5a.xlarge"
   validation {
-          condition     = ( 
-            var.acvm_instance_type == "t3.xlarge"  ||
-            var.acvm_instance_type == "m5a.xlarge" 
-          )
-          error_message = "Input acvm_instance_type must be set to an approved vm instance type."
-      }
+    condition = (
+      var.acvm_instance_type == "t3.xlarge" ||
+      var.acvm_instance_type == "m5a.xlarge"
+    )
+    error_message = "Input acvm_instance_type must be set to an approved vm instance type."
+  }
 }
 
 variable "ac_prov_key" {
-  description = "zpa app connector provisioning key"
-  type = string
+  type        = string
+  description = "ZPA App Connector provisioning key"
 }
 
 variable "associate_public_ip_address" {
-  default = false
-  type = bool
+  default     = false
+  type        = bool
   description = "enable/disable public IP addresses on App Connector instances"
 }
 
-variable "health_check_grace_period" {
-  description = "The amount of time until EC2 Auto Scaling performs the first health check on new instances after they are put into service."
-  type        = number
-  default     = 300
-}
-
 variable "min_size" {
-  description = "mininum number of cloud connectors to maintain in Autoscaling group"
   type        = number
+  description = "Mininum number of App Connectors to maintain in Autoscaling group"
   default     = 2
 }
 
 variable "max_size" {
-  description = "maxinum number of cloud connectors to maintain in Autoscaling group"
   type        = number
+  description = "Maxinum number of App Connectors to maintain in Autoscaling group"
   default     = 4
 }
 
-variable "target_value" {
-  description = "target value number for autoscaling policy CPU utilization target tracking"
+variable "health_check_grace_period" {
   type        = number
-  default     = 50
+  description = "The amount of time until EC2 Auto Scaling performs the first health check on new instances after they are put into service. Default is 5 minutes"
+  default     = 300
 }
 
 variable "warm_pool_enabled" {
+  type        = bool
   description = "If set to true, add a warm pool to the specified Auto Scaling group. See [warm_pool](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_group#warm_pool)."
   default     = "false"
-  type        = bool
 }
 
 variable "warm_pool_state" {
+  type        = string
   description = "Sets the instance state to transition to after the lifecycle hooks finish. Valid values are: Stopped (default), Running or Hibernated. Ignored when 'warm_pool_enabled' is false"
   default     = null
-  validation {
-          condition = ( 
-            var.warm_pool_state == "Stopped"  ||
-            var.warm_pool_state == "Running" ||
-            var.warm_pool_state == "Hibernated" ||
-            var.warm_pool_state == null
-          )
-          error_message = "Input warm_pool_state must be set to an approved value."
-      }
 }
 
 variable "warm_pool_min_size" {
-  description = "Specifies the minimum number of instances to maintain in the warm pool. This helps you to ensure that there is always a certain number of warmed instances available to handle traffic spikes. Ignored when 'warm_pool_enabled' is false"
   type        = number
+  description = "Specifies the minimum number of instances to maintain in the warm pool. This helps you to ensure that there is always a certain number of warmed instances available to handle traffic spikes. Ignored when 'warm_pool_enabled' is false"
   default     = null
 }
 
 variable "warm_pool_max_group_prepared_capacity" {
-  description = "Specifies the total maximum number of instances that are allowed to be in the warm pool or in any state except Terminated for the Auto Scaling group. Ignored when 'warm_pool_enabled' is false"
   type        = number
+  description = "Specifies the total maximum number of instances that are allowed to be in the warm pool or in any state except Terminated for the Auto Scaling group. Ignored when 'warm_pool_enabled' is false"
   default     = null
 }
 
 variable "reuse_on_scale_in" {
+  type        = bool
   description = "Specifies whether instances in the Auto Scaling group can be returned to the warm pool on scale in."
   default     = "false"
-  type        = bool
 }
 
 variable "launch_template_version" {
@@ -136,13 +136,24 @@ variable "launch_template_version" {
 }
 
 variable "target_tracking_metric" {
-  default = "ASGAverageCPUUtilization"
-   validation {
-          condition = ( 
-            var.target_tracking_metric == "ASGAverageCPUUtilization"  ||
-            var.target_tracking_metric == "ASGAverageNetworkIn" ||
-            var.target_tracking_metric == "ASGAverageNetworkOut"
-          )
-          error_message = "Input target_tracking_metric must be set to an approved predefined metric."
-      }
+  type        = string
+  description = "The AWS ASG pre-defined target tracking metric type. App Connector recommends ASGAverageCPUUtilization"
+  default     = "ASGAverageCPUUtilization"
+  validation {
+    condition = (
+      var.target_tracking_metric == "ASGAverageCPUUtilization" ||
+      var.target_tracking_metric == "ASGAverageNetworkIn" ||
+      var.target_tracking_metric == "ASGAverageNetworkOut"
+    )
+    error_message = "Input target_tracking_metric must be set to an approved predefined metric."
+  }
 }
+
+variable "target_cpu_util_value" {
+  type        = number
+  description = "Target value number for autoscaling policy CPU utilization target tracking. ie: trigger a scale in/out to keep average CPU Utliization percentage across all instances at/under this number"
+  default     = 50
+}
+
+
+
