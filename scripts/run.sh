17aa2a91616052f8d1d13ae7f3995d0fdc2edd6a
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-10-25 22:29:17 -0700

    Fixed CICD workflow to execute linter

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,11 @@
 
 set -euo pipefail
 
-pre-commit run --all-files terraform_fmt 
-pre-commit run --all-files terraform_docs 
+pre-commit autoupdate
+pre-commit run --all-files terraform_fmt
+pre-commit run --all-files terraform_docs
 pre-commit run --all-files terraform_tflint
+pre-commit run --all-files check-merge-conflict
+pre-commit run --all-files end-of-file-fixer
+pre-commit run --all-files gitlint
+pre-commit run --all-files zscaler-iac-scanner
