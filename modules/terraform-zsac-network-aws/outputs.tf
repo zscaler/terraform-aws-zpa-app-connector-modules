ae4bde26f11f08456b1c0b28e2eb000307b7a362
olidacombe <olidacombe@gmail.com>
2024-07-20 01:51:58 +0100

    Decouple default route from route table definitions (#16)
    Co-authored-by: Oliver Dacombe <oliver.dacombe@gsr.io[commit]>

--- modules/terraform-zsac-network-aws/outputs.tf
+++ modules/terraform-zsac-network-aws/outputs.tf
@@ -8,9 +8,19 @@ output "ac_subnet_ids" {
   value       = data.aws_subnet.ac_subnet_selected[*].id
 }
 
+output "ac_route_table_ids" {
+  description = "App Connector Route Table IDs"
+  value       = [for rt in aws_route_table.ac_rt : rt.id]
+}
+
 output "public_subnet_ids" {
   description = "Public Subnet ID"
   value       = aws_subnet.public_subnet[*].id
+}
+
+output "public_route_table_id" {
+  description = "Public Route Table ID"
+  value       = var.byo_ngw == false ? aws_route_table.public_rt[0].id : null
 }
 
 output "nat_gateway_ips" {
